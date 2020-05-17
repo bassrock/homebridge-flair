e9aa80bc48b0ae34fd130fe605e8b13c88b75426
Daniel Brooks <bass_rock@me.com>
2020-05-17 11:33:30 -0700

    add device types

--- src/ventPlatformAccessory.ts
+++ src/ventPlatformAccessory.ts
@@ -12,19 +12,30 @@ import {Vent} from 'flair-api-ts/lib/client/models';
 import {Pressure, PressureSensor} from './Pressure';
 import {getRandomIntInclusive} from './utils';
 
+enum AccessoryType {
+    WindowCovering = 'windowCovering',
+    Fan = 'fan',
+    AirPurifier = 'airPurifier'
+}
+
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
 export class FlairVentPlatformAccessory {
-    private windowService: Service;
+    private fanService?: Service;
+    private windowService?: Service;
+    private airPurifierService?: Service;
+    private mainService: Service;
+
     private temperatureService: Service;
     private pressureService: Service;
     private accessoryInformationService: Service;
 
     private vent: Vent;
     private client: Client;
+    private accessoryType: AccessoryType;
 
     constructor(
         private readonly platform: FlairPlatform,
@@ -33,6 +44,10 @@ export class FlairVentPlatformAccessory {
     ) {
       this.vent = this.accessory.context.device;
       this.client = client;
+      this.accessoryType = this.platform.config.ventAccessoryType as AccessoryType;
+      if (!this.accessoryType) {
+        this.accessoryType = AccessoryType.WindowCovering;
+      }
 
       // set accessory information
       this.accessoryInformationService = this.accessory.getService(this.platform.Service.AccessoryInformation)!
@@ -40,49 +55,107 @@ export class FlairVentPlatformAccessory {
         .setCharacteristic(this.platform.Characteristic.Model, 'Vent')
         .setCharacteristic(this.platform.Characteristic.SerialNumber, this.vent.id!);
 
+      this.fanService = this.accessory.getService(this.platform.Service.Fanv2);
+      this.windowService = this.accessory.getService(this.platform.Service.WindowCovering);
+      this.airPurifierService = this.accessory.getService(this.platform.Service.AirPurifier);
+
       // We fake a vent as a window covering.
-      this.windowService = this.accessory.getService(this.platform.Service.WindowCovering)
-          ?? this.accessory.addService(this.platform.Service.WindowCovering);
-      this.windowService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
-      this.windowService.setPrimaryService(true);
-      this.windowService.getCharacteristic(this.platform.Characteristic.TargetPosition).setProps({
-        minStep: 50,
-      });
-      this.windowService.getCharacteristic(this.platform.Characteristic.CurrentPosition).setProps({
-        minStep: 50,
-      });
+      switch (this.accessoryType) {
+        case AccessoryType.WindowCovering:
+          if (this.fanService) {
+            this.accessory.removeService(this.fanService);
+          }
+
+          if (this.airPurifierService) {
+            this.accessory.removeService(this.airPurifierService);
+          }
+
+          this.windowService = this.windowService
+                    ?? this.accessory.addService(this.platform.Service.WindowCovering);
+          this.windowService.getCharacteristic(this.platform.Characteristic.TargetPosition).setProps({
+            minStep: 50,
+          });
+          this.windowService.getCharacteristic(this.platform.Characteristic.CurrentPosition).setProps({
+            minStep: 50,
+          });
+          this.windowService.setCharacteristic(this.platform.Characteristic.TargetPosition, this.vent.percentOpen);
+          this.windowService.setCharacteristic(this.platform.Characteristic.CurrentPosition, this.vent.percentOpen);
+          this.windowService.setCharacteristic(
+            this.platform.Characteristic.PositionState,
+            this.platform.Characteristic.PositionState.STOPPED,
+          );
+          this.windowService.getCharacteristic(this.platform.Characteristic.TargetPosition)
+            .on(CharacteristicEventTypes.SET, this.setTargetPosition.bind(this))
+            .on(CharacteristicEventTypes.GET, this.getTargetPosition.bind(this));
+          this.mainService = this.windowService;
+          break;
+        case AccessoryType.AirPurifier:
+          if (this.fanService) {
+            this.accessory.removeService(this.fanService);
+          }
+
+          if (this.windowService) {
+            this.accessory.removeService(this.windowService);
+          }
+
+          this.airPurifierService = this.airPurifierService
+                    ?? this.accessory.addService(this.platform.Service.AirPurifier);
+
+          this.airPurifierService.getCharacteristic(this.platform.Characteristic.RotationSpeed).setProps({
+            minStep: 50,
+          });
+          this.airPurifierService.getCharacteristic(this.platform.Characteristic.RotationSpeed)
+            .on(CharacteristicEventTypes.SET, this.setTargetPosition.bind(this))
+            .on(CharacteristicEventTypes.GET, this.getTargetPosition.bind(this));
+          this.mainService = this.airPurifierService;
+          break;
+        case AccessoryType.Fan:
+          if (this.airPurifierService) {
+            this.accessory.removeService(this.airPurifierService);
+          }
+
+          if (this.windowService) {
+            this.accessory.removeService(this.windowService);
+          }
+          this.fanService =  this.fanService
+                    ?? this.accessory.addService(this.platform.Service.Fanv2);
+
+          this.fanService.getCharacteristic(this.platform.Characteristic.RotationSpeed).setProps({
+            minStep: 50,
+          });
+
+          this.fanService.getCharacteristic(this.platform.Characteristic.RotationSpeed)
+            .on(CharacteristicEventTypes.SET, this.setTargetPosition.bind(this))
+            .on(CharacteristicEventTypes.GET, this.getTargetPosition.bind(this));
+          this.mainService = this.fanService;
+          break;
+        default:
+          throw Error('No Vent Accessory Type Selected.');
+          break;
+      }
+
+      this.mainService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+      this.mainService.setPrimaryService(true);
 
       //Add our temperature sensor
       this.temperatureService = this.accessory.getService(this.platform.Service.TemperatureSensor)
-          ?? this.accessory.addService(this.platform.Service.TemperatureSensor);
+            ?? this.accessory.addService(this.platform.Service.TemperatureSensor);
       this.temperatureService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
       this.temperatureService.setCharacteristic(
         this.platform.Characteristic.CurrentTemperature,
         this.vent.ductTemperatureC,
       );
-      this.windowService.addLinkedService(this.temperatureService);
+      this.mainService.addLinkedService(this.temperatureService);
 
       //Add our custom pressure sensor
       this.pressureService = this.accessory.getService(PressureSensor) ?? this.accessory.addService(PressureSensor);
       this.pressureService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
       this.pressureService.setCharacteristic(Pressure, this.vent.ductPressure);
-      this.windowService.addLinkedService(this.pressureService);
-
-      this.windowService.setCharacteristic(this.platform.Characteristic.TargetPosition, this.vent.percentOpen);
-      this.windowService.setCharacteristic(this.platform.Characteristic.CurrentPosition, this.vent.percentOpen);
-      this.windowService.setCharacteristic(
-        this.platform.Characteristic.PositionState,
-        this.platform.Characteristic.PositionState.STOPPED,
-      );
-      this.windowService.getCharacteristic(this.platform.Characteristic.TargetPosition)
-        .on(CharacteristicEventTypes.SET, this.setTargetPosition.bind(this))
-        .on(CharacteristicEventTypes.GET, this.getTargetPosition.bind(this));
-
-
+      this.mainService.addLinkedService(this.pressureService);
 
       setInterval(async () => {
         await this.getNewVentReadings();
-      }, (platform.config.pollInterval+ getRandomIntInclusive(1,20)) * 1000);
+      }, (platform.config.pollInterval + getRandomIntInclusive(1, 20)) * 1000);
       this.getNewVentReadings();
     }
 
@@ -129,12 +202,30 @@ export class FlairVentPlatformAccessory {
 
       this.pressureService.updateCharacteristic(Pressure, this.vent.ductPressure);
 
-      this.windowService.updateCharacteristic(this.platform.Characteristic.TargetPosition, this.vent.percentOpen);
-      this.windowService.updateCharacteristic(this.platform.Characteristic.CurrentPosition, this.vent.percentOpen);
-      this.windowService.updateCharacteristic(
-        this.platform.Characteristic.PositionState,
-        this.platform.Characteristic.PositionState.STOPPED,
-      );
+      // We fake a vent as a window covering.
+      switch (this.accessoryType) {
+        case AccessoryType.WindowCovering:
+          this.mainService.updateCharacteristic(this.platform.Characteristic.TargetPosition, this.vent.percentOpen);
+          this.mainService.updateCharacteristic(this.platform.Characteristic.CurrentPosition, this.vent.percentOpen);
+          this.mainService.updateCharacteristic(
+            this.platform.Characteristic.PositionState,
+            this.platform.Characteristic.PositionState.STOPPED,
+          );
+          break;
+        case AccessoryType.AirPurifier:
+          this.mainService.updateCharacteristic(this.platform.Characteristic.RotationSpeed, this.vent.percentOpen);
+          this.mainService.updateCharacteristic(this.platform.Characteristic.Active, this.vent.percentOpen > 0);
+          this.mainService.updateCharacteristic(this.platform.Characteristic.CurrentAirPurifierState, this.vent.percentOpen > 0);
+          break;
+        case AccessoryType.Fan:
+          this.mainService.updateCharacteristic(this.platform.Characteristic.RotationSpeed, this.vent.percentOpen);
+          this.mainService.updateCharacteristic(this.platform.Characteristic.Active, this.vent.percentOpen > 0);
+          break;
+        default:
+          throw Error('No Vent Accessory Type Selected.');
+          break;
+      }
+
 
       this.accessoryInformationService.updateCharacteristic(
         this.platform.Characteristic.FirmwareRevision,
